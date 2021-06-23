f147e6aa9cbdf478d9037aaedb6a40dd96cbb1cb
Carlos Rincones <cri@facelift-bbt.com>
2021-06-23 22:34:03 +0200

    add code to make comments

--- src/Picshare.elm
+++ src/Picshare.elm
@@ -3,12 +3,12 @@ module Picshare exposing (main)
 import Browser
 import Dict exposing (update)
 import Html exposing (..)
-import Html.Attributes exposing (class, src)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (class, disabled, placeholder, src, type_, value)
+import Html.Events exposing (onClick, onInput, onSubmit)
 
 
 type alias Model =
-    { url : String, caption : String, liked : Bool }
+    { url : String, caption : String, liked : Bool, comments : List String, newComment : String }
 
 
 baseUrl : String
@@ -21,6 +21,8 @@ initialModel =
     { url = baseUrl ++ "1.jpg"
     , caption = "Party time cool dude"
     , liked = False
+    , comments = [ "Hey man" ]
+    , newComment = ""
     }
 
 
@@ -39,6 +41,41 @@ viewLoveButton model =
         ]
 
 
+viewComment : String -> Html Msg
+viewComment comment =
+    li []
+        [ strong [] [ text "Comment: " ]
+        , text ("" ++ comment)
+        ]
+
+
+viewCommentList : List String -> Html Msg
+viewCommentList comments =
+    case comments of
+        [] ->
+            text ""
+
+        _ ->
+            div [ class "comments" ] [ ul [] (List.map viewComment comments) ]
+
+
+viewComments : Model -> Html Msg
+viewComments model =
+    div []
+        [ viewCommentList model.comments
+        , form [ class "new-comment", onSubmit SaveComment ]
+            [ input
+                [ type_ "text"
+                , placeholder "Add a comment..."
+                , value model.newComment
+                , onInput UpdateComment
+                ]
+                []
+            , button [ disabled (String.isEmpty model.newComment) ] [ text "Save" ]
+            ]
+        ]
+
+
 viewDetailedPhoto : Model -> Html Msg
 viewDetailedPhoto model =
     div [ class "detailed-photo" ]
@@ -46,6 +83,7 @@ viewDetailedPhoto model =
         , div [ class "photo-info" ]
             [ viewLoveButton model
             , h2 [ class "caption" ] [ text model.caption ]
+            , viewComments model
             ]
         ]
 
@@ -74,6 +112,22 @@ main =
 
 type Msg
     = ToggleLike
+    | UpdateComment String
+    | SaveComment
+
+
+saveNewComment : Model -> Model
+saveNewComment model =
+    let
+        comment =
+            String.trim model.newComment
+    in
+    case comment of
+        "" ->
+            model
+
+        _ ->
+            { model | comments = model.comments ++ [ comment ], newComment = "" }
 
 
 update : Msg -> Model -> Model
@@ -81,3 +135,9 @@ update msg model =
     case msg of
         ToggleLike ->
             { model | liked = not model.liked }
+
+        UpdateComment comment ->
+            { model | newComment = comment }
+
+        SaveComment ->
+            saveNewComment model
